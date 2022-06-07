c21b82c7f1bbb94296785a7005bbdb32fae37e51
dogun <yuexuqiang@gmail.com>
2022-06-07 14:54:14 +0800

    int to int32_t for data

--- esp32/main/stereo2mono.h
+++ esp32/main/stereo2mono.h
@@ -8,12 +8,12 @@
 #ifndef MAIN_STEREO2MONO_H_
 #define MAIN_STEREO2MONO_H_
 
-static inline void stereo2mono(int* src, int* dst, int len) {
+static inline void stereo2mono(int32_t* src, int32_t* dst, int len) {
 	int i;
 
 	for (i = 0; i < len; i += 2) {
-		int rl = src[i];
-		int rr = src[i+1];
+		int32_t rl = src[i];
+		int32_t rr = src[i+1];
 
 		rl = rl >> 8;
 		rr = rr >> 8;
